6fbe7626e271bafb764b6992b98a2919009bd93b
Alex Mark <ajmark@andrew.cmu.edu>
2014-03-05 21:14:52 -0500

    rm

--- models/mongo_dinex.js
+++
@@ -1,9 +0,0 @@
-var util = require("util");
-var mongoClient = require("mongodb").MongoClient;
-var server = "mongodb://localhost:27017/";
-
-//db/:collection/:operation/:document
-var doError = function(e) {
-        util.debug("ERROR: " + e);
-        throw new Error(e);
-    }
